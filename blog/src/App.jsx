7c1e25492338139ce806e937a9bfd0b5b7f0fe5e
Binz <singriet0824@hs.ac.kr>
2025-08-05 07:35:32 +0000

    feat:  4강 숙제 - useState

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  let [제목리스트] = useState(['남자코드 추천', '강남 우동 맛집', '파이썬 독학']);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <div className='black-nav'>
+          <h4>블로그임</h4>
+        </div>
+
+
+      <div className='list'>
+        <h4>{제목리스트[0]}</h4>
+        <p>2월 17일 발행</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='list'>
+        <h4>{제목리스트[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>  
+      
+      <div className='list'>  
+        <h4>{제목리스트[2]}</h4>
+        <p>2월 17일 발행</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
+      </div>
     </>
   )
 }
