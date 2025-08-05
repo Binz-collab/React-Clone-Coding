4ad3f98ca8414f077d7bad17b1ff9038bc4b7082
Binz <singriet0824@hs.ac.kr>
2025-08-05 13:22:42 +0000

    feat:  14강 응용 - 빈 제목 게시글 추가 방지 + 날짜데이터 넣어주기 (다른 게시글 누르면 모달창 안딛히고 바로 보이게  + 리스트 비어도 모달창 안닫힘 버그 수정 필요)

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -2,11 +2,25 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
+  let today = new Date();
+
+  let year = today.getFullYear();
+  let month = ('0' + (today.getMonth() + 1)).slice(-2);
+  let day = ('0' + today.getDate()).slice(-2);
+  let dateString = year + '년 ' + month  + '월 ' + day + '일 ';
+
+  let hours = ('0' + today.getHours()).slice(-2); 
+  let minutes = ('0' + today.getMinutes()).slice(-2);
+  let seconds = ('0' + today.getSeconds()).slice(-2); 
+  let timeString = hours + ':' + minutes  + ':' + seconds;
+
   let [select, setSelect] = useState(-1);
   let [제목리스트, 제목리스트변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉수, 따봉수변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(true);
-  let input = ''; 
+  let [생성날짜, 생성날짜변경] = useState([dateString, dateString, dateString]);
+  let [생성시간, 생성시간변경] = useState(([timeString, timeString, timeString]));
+  let input = '';
 
   return (
     <>
@@ -40,17 +54,25 @@ function App() {
                   let 삭제따봉수 = [...따봉수];
                   삭제따봉수.splice(i, 1);
                   따봉수변경(삭제따봉수);
+
+                  let 삭제날짜 = [...생성날짜];
+                  삭제날짜.splice(i, 1);
+                  생성날짜변경(삭제날짜);
+
+                  let 삭제시간 = [...생성시간];
+                  삭제시간.splice(i, 1);
+                  생성시간변경(삭제시간);
                 }}>삭제</button>
 
               </h4>
-              <p>2월 17일 발행</p>
+              <p>{ 생성날짜[i] }</p>
             </div>
           )
         })
       }
 
       {
-        modal && (제목리스트.length > 0) ? <Modal i={select} 제목={제목리스트} 제목변경={제목리스트변경}/> : null
+        modal && (제목리스트.length > 0) ? <Modal i={select} 제목={제목리스트} 제목변경={제목리스트변경} 시간={생성시간}/> : null
       }
 
       <input onChange={(e) => {
@@ -58,13 +80,33 @@ function App() {
       }}></input>
     
       <button onClick={() => {
-        let 뉴제목리스트 = [...제목리스트];
-        뉴제목리스트.unshift(input);       //원본을 바로 바꿈
-        제목리스트변경(뉴제목리스트);
+        if (input != '') {
+          let 뉴제목리스트 = [...제목리스트];
+          뉴제목리스트.unshift(input);       //원본을 바로 바꿈
+          제목리스트변경(뉴제목리스트);
 
-        let 뉴따봉수 = [...따봉수];
-        뉴따봉수.unshift(0);
-        따봉수변경(뉴따봉수);
+          let 뉴따봉수 = [...따봉수];
+          뉴따봉수.unshift(0);
+          따봉수변경(뉴따봉수);
+
+          year = today.getFullYear();
+          month = ('0' + (today.getMonth() + 1)).slice(-2);
+          day = ('0' + today.getDate()).slice(-2);
+          dateString = year + '년 ' + month  + '월 ' + day + '일 ';
+
+          let 뉴생성날짜 = [...생성날짜];
+          뉴생성날짜.unshift(dateString);
+          생성날짜변경(뉴생성날짜);
+
+          hours = ('0' + today.getHours()).slice(-2); 
+          minutes = ('0' + today.getMinutes()).slice(-2);
+          seconds = ('0' + today.getSeconds()).slice(-2); 
+          timeString = hours + ':' + minutes  + ':' + seconds;
+
+          let 뉴생성시간 = [...생성시간];
+          뉴생성시간.unshift(timeString);
+          생성시간변경(뉴생성시간);
+        }
       }}>글 추가하기</button>
       
         
@@ -79,7 +121,7 @@ function Modal(props) {
   return (
     <div className='modal'>
       <h4>{ props.i < 0 ? props.제목[0]: props.제목[props.i] }</h4>
-      <p>날짜</p>
+      <p>{ props.i < 0 ? props.시간[0] : props.시간[props.i] }</p>
       <p>설명</p>
 
       <button onClick={() => props.제목변경(['여자 코트 추천', '강북 냉면 맛집', '리액트 인강'])}>제목변경2</button>
