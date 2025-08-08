53fd39f77f96869f422e18cfdd0a47ff9885508a
Binz <singriet0824@hs.ac.kr>
2025-08-08 05:18:53 +0000

    Test:  글 추가하기 테스트 코드 작성 및 실행 (결과적으로 실패)

--- blog/src/App.test.jsx
+++ blog/src/App.test.jsx
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders blog title', () => {
   render(<App />);
   const linkElement = screen.getByText(/블로그임/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('adds a new post', async () => {
+  const user = userEvent.setup();
+  render(<App />);
+
+  const input = screen.getByRole('textbox');
+  const addButton = screen.getByRole('button', { name: /글 추가하기/i });
+
+  const newPostTitle = '새로운 포스트 제목';
+  await user.type(input, newPostTitle);
+  await user.click(addButton);
+
+  const newPostElement = await screen.findByText(newPostTitle);
+  expect(newPostElement).toBeInTheDocument();
 });
