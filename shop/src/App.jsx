6d57714fbd45df0fbdfbe3b5799c971b7b449968
Binz <singriet0824@hs.ac.kr>
2025-08-13 10:39:54 +0000

    feat:  상품 목록 구현

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -18,6 +18,36 @@ function App() {
             </Nav>
           </Container>
         </Navbar>
+
+        <div className='main-bg' />
+
+        <Container>
+          <div className='row text-center'>
+            <div className='col-md-4'>
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="Product 1" width="80%" />
+              <h3>Product 1</h3>
+              <p>Description of Product 1</p>
+              <Button variant="primary">Buy Now</Button>
+            </div>
+            <div className='col-md-4'>
+              <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="Product 2" width="80%" />
+              <h3>Product 2</h3>
+              <p>Description of Product 2</p>
+              <Button variant="primary">Buy Now</Button>
+            </div>
+            <div className='col-md-4'>
+              <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="Product 3" width="80%" />
+              <h3>Product 3</h3>
+              <p>Description of Product 3</p>
+              <Button variant="primary">Buy Now</Button>
+            </div>
+          </div>
+        </Container>
+        {/* <Container className="mt-5">
+          <h1>Welcome to AppleShop</h1>
+          <p>Your one-stop shop for all things Apple!</p>
+          <Button variant="primary">Shop Now</Button>
+        </Container> */}
         
       </div>
     </>
